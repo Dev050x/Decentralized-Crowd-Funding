8b6a346c8b2a2faa0881a9a5e3452f43a15c0a3d
divPatel <divpatel4081@gmail.com>
2024-11-25 03:09:54 +0600

    basic structure of dashboard is done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import { useReadContract } from "thirdweb/react";
 import { client } from "./client";
 import { baseSepolia } from "thirdweb/chains";
 import { getContract } from "thirdweb";
-// import { CampaignCard } from "@/components/CampaignCard";
 import { CROWDFUNDING_FACTORY } from "./constants/contracts";
+import CampaignCard from "./components/CampaignCard";
 
 export default function Home() {
   const contract = getContract({
@@ -26,6 +26,20 @@ export default function Home() {
     <main className="mx-auto max-w-7xl px-4 mt-4 sm:px-6 lg:px-8">
       <div className="py-10">
         <h1 className="text-4xl font-bold mb-4">Campaigns:</h1>
+        <div className="grid grid-cols-3 gap-4">
+          {!isLoadingCampaigns && campaigns && (
+            campaigns.length > 0 ? (
+              campaigns.map((campaign) => (
+                <CampaignCard
+                  key={campaign.campaignAddress}
+                  campaignAddress={campaign.campaignAddress}
+                />
+              ))
+            ) : (
+              <p>No Campaigns</p>
+            )
+          )}
+        </div>
       </div>
     </main>
 
